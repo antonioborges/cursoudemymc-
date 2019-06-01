88ce9ac58804e1bb1ba2d07206e3cf0e97e98188
antonioborges <asampaio_borges@yahoo.com.br>
2019-06-01 18:39:36 -0300

    projeto criado

---
+++ src/main/java/br/com/codigolivre/cursoudemymc/CursoudemymcApplication.java
@@ -0,0 +1,13 @@
+package br.com.codigolivre.cursoudemymc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoudemymcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoudemymcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codigolivre/cursoudemymc/CursoudemymcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codigolivre.cursoudemymc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoudemymcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
